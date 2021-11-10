b90067ea579788baa07cbdaabcfc08dfda25231c
ViniciusMoreno123 <93357216+ViniciusMoreno123@users.noreply.github.com>
2021-11-09 20:45:54 -0400

    Aula 13 09/11/2021
    Tempodejogatina

--- sketch.js
+++ sketch.js
@@ -2,19 +2,21 @@ var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 var clouds;
 var imgclouds;
-
-
 var score;
-
+var obs1,obs2,obs3,obs4,obs5,obs6;
+var Tempodejogo;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
   trex_collided = loadImage("trex_collided.png");
   imgclouds= loadImage("cloud.png");
   groundImage = loadImage("ground2.png");
-  
- 
-  
+  obs1 = loadImage("obstacle1.png");
+  obs2 = loadImage("obstacle2.png");
+  obs3 = loadImage("obstacle3.png");
+  obs4 = loadImage("obstacle4.png");
+  obs5 = loadImage("obstacle5.png");
+  obs6 = loadImage("obstacle6.png");
 }
 
 function setup() {
@@ -35,22 +37,24 @@ function setup() {
   //crie um solo invisível
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
+
+  console.log(5+"     Oi");
   
   //gerar números aleatórios
   var rand =  Math.round(random(1,100))
-  console.log(rand)
+  //console.log(rand)
 
+Tempodejogo = 0;
 }
 
 function draw() {
-console.log(frameCount);
+//console.log(frameCount);
   //definir cor do plano de fundo
   background(180);
-  
-  console.log(trex.y)
-  
-  
-  
+  //console.log(trex.y)
+text ("Tempo De Jogatina:"+Tempodejogo,250,50);
+Tempodejogo = Tempodejogo+Math.round(frameCount/60);
+
   // pulando o trex ao pressionar a tecla de espaço
   if(keyDown("space")&& trex.y >= 150) {
     trex.velocityY = -10;
@@ -66,16 +70,14 @@ console.log(frameCount);
   trex.collide(invisibleGround);
   
   //Gerar Nuvens
-  spawnClouds()
-  
+  spawnClouds();
+  gerarobstaculos();
   drawSprites();
 }
 
 //função para gerar as nuvens
 function spawnClouds(){
  //escreva seu código aqui
-
-
 
 if(frameCount%60 ===0){
   clouds= createSprite(600,100,40,10);
@@ -84,9 +86,47 @@ if(frameCount%60 ===0){
   clouds.depth = trex.depth;
   trex.depth = trex.depth+1;
   clouds.velocityX= -3;
+
+  clouds.lifetime = 250;
 }
 
+}
+function gerarobstaculos(){
+  if(frameCount%60 ===0){
+var obstaculos = createSprite(600,165,10,40);
+obstaculos.velocityX =-6;
+var numeroRan = Math.round(random (1,6));
+switch(numeroRan){
+case 1:
+  obstaculos.addImage(obs1);
+break
 
+case 2:
+  obstaculos.addImage(obs2);
+  break
+
+  case 3:
+  obstaculos.addImage(obs3);
+  break
+
+  case 4:
+  obstaculos.addImage(obs4);
+ break
+
+ case 5:
+ obstaculos.addImage(obs5);
+ break
+
+ case 6:
+ obstaculos.addImage(obs6);
+break
+
+default:break
+}
+obstaculos.scale =0.5;
+
+obstaculos.lifetime = 300;
+  }
 
 
 }
